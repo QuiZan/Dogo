a4d5ca47fe9daf30ea84e018048a9b01da465c1e
QuiZan <biskupskif@gmail.com>
2021-04-04 22:20:46 +0200

    Dodaj pliki projektów.

---
+++ Dogo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Dogo.Areas.Identity.Data;
+using Dogo.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Dogo.Areas.Identity.IdentityHostingStartup))]
+namespace Dogo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DogoContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DogoContextConnection")));
+
+                services.AddDefaultIdentity<DogoUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DogoContext>();
+            });
+        }
+    }
+}
